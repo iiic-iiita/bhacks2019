14e5a5910cd49cd3c77c2d41ee1690eaf1c4c718
prateek <prateek7672168@gmail.com>
2019-03-09 18:06:57 +0530

    Update localdatabase.sql

--- searching for team name/sql code/localdatabase.sql
+++ searching for team name/sql code/localdatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 03, 2018 at 06:29 AM
+-- Generation Time: Mar 09, 2019 at 11:45 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -34,13 +34,13 @@ CREATE TABLE `applications` (
   `rollNumber` varchar(255) NOT NULL,
   `branch` varchar(255) NOT NULL,
   `semester` int(11) NOT NULL,
-  `startDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `endDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `startDate` varchar(256) NOT NULL,
+  `endDate` varchar(256) NOT NULL,
   `natureOfLeave` varchar(255) NOT NULL,
   `purpose` varchar(255) NOT NULL,
   `classScheduledOnLeave` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `mobile` int(11) NOT NULL,
+  `mobile` varchar(256) NOT NULL,
   `email` varchar(255) NOT NULL,
   `uploadedImageName` varchar(255) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0'
@@ -51,11 +51,10 @@ CREATE TABLE `applications` (
 --
 
 INSERT INTO `applications` (`id`, `studentName`, `rollNumber`, `branch`, `semester`, `startDate`, `endDate`, `natureOfLeave`, `purpose`, `classScheduledOnLeave`, `address`, `mobile`, `email`, `uploadedImageName`, `status`) VALUES
-(1, 'prateek', 'iec2017029', 'ece', 3, '2018-08-25 18:30:00', '2018-08-25 18:30:00', 'mood', 'mood', 'yes', '1111 bh4', 999999999, 'iec2017029@iiita.ac.in', 'cr7', 1),
-(2, 'asfhgn', 'rfthjyrdgth', 'ece', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'fhrty6uh', 'ytrhg', 'rytuujhr', 'yjthgjyt', 0, 'ryjh@ghtdgh', '5b836cb74f0468.32620900.jpg', 1),
-(4, 'test', '', '', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', '', '', 0, '', '', 2),
-(5, 'test', '', 'ece', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', '', '5378    BH5', 2147483647, 'iec2017029@iiita.ac.in', '', 3),
-(6, 'prateek', 'iec2017029', 'ece', 3, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'ec', 'fghn', 'fdgnh', '5378    BH5', 2147483647, 'iec2017029@iiita.ac.in', '', 0);
+(54, 'prateek', 'iec2017029', 'ECE', 4, '2019-03-09', '2019-03-20', 'IFSC2017029', 'BOI', 'PRATEEK', 'LALPUR', 'BH4', 'iec2017029@iiita.ac.in', '', 3),
+(56, 'John doe', 'iec2012029', 'IT', 4, '2019-03-22', '2019-03-23', 'DATA_STRUCTURE', 'ZERO_MERCY', 'popo', 'ALLAHABAD', 'BH1', 'iec2012029@iiita.ac.in', '', 0),
+(57, 'jhon doe', 'iec2012029', 'IT', 4, '2019-03-22', '2019-03-23', 'jbjb', 'ZERO_MERCY', 'popo', 'ranchi', 'BH1', 'iec2012029@iiita.ac.in', '', 0),
+(58, 'jhon doe', 'iec2012029', 'IT', 6, '2019-03-27', '2019-03-31', 'DATA_STRUCTURE', 'ZERO_MERCY', 'ergte', 'ALLAHABAD', 'BH1', 'iec2012029@iiita.ac.in', '', 0);
 
 -- --------------------------------------------------------
 
@@ -67,6 +66,7 @@ CREATE TABLE `users` (
   `id` int(200) NOT NULL,
   `Name` varchar(300) NOT NULL,
   `Enroll` varchar(200) NOT NULL,
+  `HOSTEL` varchar(11) NOT NULL,
   `Email` varchar(300) NOT NULL,
   `Password` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -75,9 +75,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `Name`, `Enroll`, `Email`, `Password`) VALUES
-(7, 'John', 'IIM2017008', 'iim2017008@iiita.ac.in', '527bd5b5d689e2c32ae974c6229ff785'),
-(8, 'los balancos', 'iec2017029', 'iec2017029@iiita.ac.in', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `users` (`id`, `Name`, `Enroll`, `HOSTEL`, `Email`, `Password`) VALUES
+(7, 'John', 'IIM2017008', 'BH4', 'iim2017008@iiita.ac.in', '527bd5b5d689e2c32ae974c6229ff785'),
+(8, 'los balancos', 'iec2017029', 'BH4', 'iec2017029@iiita.ac.in', '202cb962ac59075b964b07152d234b70'),
+(15, 'john doe', 'iec2012029', 'BH1', 'iec2012029@iiita.ac.in', '202cb962ac59075b964b07152d234b70');
 
 --
 -- Indexes for dumped tables
@@ -103,12 +104,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `applications`
 --
 ALTER TABLE `applications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;COMMIT;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
